5dc7d6355b04cbfe6798c805c9061276287a1259
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2021-02-24 17:32:01 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
+
