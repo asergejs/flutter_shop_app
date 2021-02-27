89b6b80a574cce7ac83d9221cea90254a935bfd2
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2021-02-27 09:59:00 +0200

    191 - provider implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/screens/product_detail_screen.dart';
 import 'package:shop/screens/products_overview_screen.dart';
+
+import './providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,18 +13,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Lato',
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: 'Lato',
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          '/' : (ctx) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName : (ctx) => ProductDetailScreen()
+        },
       ),
-      routes: {
-        '/' : (ctx) => ProductsOverviewScreen(),
-        ProductDetailScreen.routeName : (ctx) => ProductDetailScreen()
-      },
     );
   }
 }
