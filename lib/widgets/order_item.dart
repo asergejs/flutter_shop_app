ba4c8fccd69d4ee0d0e4e8504bab1e38c495da9d
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2021-03-07 09:31:03 +0200

    209 - making orders expandable

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:shop/providers/orders_provider.dart' as ord;
 import 'package:intl/intl.dart';
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
 
   OrderItem(this.order);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +22,43 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(DateFormat('dd MM yyyy').format(order.dateTime)),
-            trailing: IconButton(icon: Icon(Icons.expand_more), onPressed: () {}),
-          )
+            title: Text('\$${widget.order.amount}'),
+            subtitle:
+                Text(DateFormat('dd MM yyyy').format(widget.order.dateTime)),
+            trailing: IconButton(
+                icon: Icon(
+                  _expanded ? Icons.expand_less : Icons.expand_more,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                }),
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20.0 + 10, 180),
+              child: ListView(
+                children: widget.order.products
+                    .map((product) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              product.title,
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${product.quantity}x \$${product.price}',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
