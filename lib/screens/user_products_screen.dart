d2df8ebc38963778edebd8aad653546438086cc5
Sergejs Arapovs <sergejs.arapovs@wandoofinance.com>
2021-04-25 09:28:56 +0300

    250 - refresh indicator

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -8,6 +8,10 @@ import 'package:shop/widgets/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
+  Future<void> _refreshProducts(BuildContext context) async {
+    await Provider.of<ProductsProvider>(context, listen: false).fetchAndSetProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsProvider = Provider.of<ProductsProvider>(context);
@@ -25,20 +29,23 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-          itemBuilder: (ctx, idx) => Column(
-            children: [
-              UserProductItem(
-                productsProvider.items[idx].id,
-                productsProvider.items[idx].title,
-                productsProvider.items[idx].imageUrl,
-              ),
-              Divider(),
-            ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshProducts(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListView.builder(
+            itemBuilder: (ctx, idx) => Column(
+              children: [
+                UserProductItem(
+                  productsProvider.items[idx].id,
+                  productsProvider.items[idx].title,
+                  productsProvider.items[idx].imageUrl,
+                ),
+                Divider(),
+              ],
+            ),
+            itemCount: productsProvider.items.length,
           ),
-          itemCount: productsProvider.items.length,
         ),
       ),
     );
